5ac92b5c7619e461f6723cd9b8361c13a92d3b47
keshav-hue <baplawatkeshav@gmail.com>
2024-09-30 17:09:54 +0530

    Add files via upload

---
+++ loading.h
@@ -0,0 +1,21 @@
+#include<iostream>
+#include <unistd.h>
+#include <cstdlib>
+using namespace std;
+
+void loadingBar()
+{
+	system("clear");
+	
+	
+
+	cout<<"\n\t\t\t\t\tProject By Keshav Sharma";
+	cout<<"\n\n\n\t\t\t\t\tLoading...\n";
+	cout<<endl;
+	cout<<"\t\t\t\t\t";
+	cout<<"\r";
+	cout<<"\t\t\t\t\t";
+	sleep(1);
+	
+}
+
